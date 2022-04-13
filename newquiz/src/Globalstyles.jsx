b3fbe90fa35e2643bf46d0f07c5539cd8a77cbfa
Mikail Sayan <msayan@outlook.de>
2022-04-13 10:55:13 +0200

    Create Structur1

---
+++ newquiz/src/Globalstyles.jsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  background-color: #f8f8f8f8;
+  color: #193151;
+  font-family: "Helvetica Neue", sans-serif;
+}
+`;
